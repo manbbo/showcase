5c5faef3ff0806d235f1eaddc2bc2c2cec0f2e3c
Piton <andre.elias.vieira@gmail.com>
2020-10-08 13:50:02 -0300

    Final touches, finishing game and adding local saving

--- Assets/Resources/Scripts/ClothesInfo.cs
+++ Assets/Resources/Scripts/ClothesInfo.cs
@@ -6,7 +6,18 @@ public class Models
 {
 	// classe que recebe as propriedades do objeto "models" que está no JSon
 	public string name;
-	public List<float> position, rotation, scale;
+	public List<int> position, rotation, scale;
+
+	public Models(string name, 
+		List<int> position, 
+		List<int> rotation, 
+		List<int> scale)
+    {
+		this.name = name;
+		this.position = position;
+		this.rotation = rotation;
+		this.scale = scale;
+    }
 }
 
 [System.Serializable]
--- Assets/Resources/Scripts/SaveScene.cs
+++ Assets/Resources/Scripts/SaveScene.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class SaveScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private ClothesInfo clothesInfo = new ClothesInfo();
+
+    public void SaveIntoJson()
     {
-        
+        clothesInfo.models = new List<Models>();
+        foreach (var go in GameObject.FindGameObjectsWithTag("Model"))
+        {
+            string name = go.name;
+            List<int> position = new List<int>() { (int)go.transform.position.x, (int)go.transform.position.y, (int)go.transform.position.z },
+                rotation = new List<int>() { (int)go.transform.rotation.x, (int)go.transform.rotation.y, (int)go.transform.rotation.z },
+                scale = new List<int>() { (int)go.transform.localScale.x, (int)go.transform.localScale.y, (int)go.transform.localScale.z };
+
+            clothesInfo.models.Add(new Models(name, position, rotation, scale));
+        }
+
+        string clothes = JsonUtility.ToJson(clothesInfo);
+        System.IO.File.WriteAllText(Application.persistentDataPath + "/models.json", clothes);
+        Debug.Log(Application.persistentDataPath);
+        // Start is called before the first frame update
     }
 }
